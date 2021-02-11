4e902544d4ddf6d839e2dc0759e735b2a2386c35
Susana Arroyo <sarroyo1@ucol.mx>
2021-02-10 20:22:46 -0600

    Proyecto BreakingBad

---
+++ Data/BBCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+namespace BreakingBad.Data
+{
+    public class BBCharacter
+    {
+        public int char_id { get; set; }
+        public string name { get; set; }
+        public string birthday { get; set; }
+        public List<string> occupation { get; set; }
+        public string img { get; set; }
+        public string status { get; set; }
+        public string nickname { get; set; }
+        // public List<int> appearance { get; set; }
+        public string portrayed { get; set; }
+
+    }
+}
---
+++ Data/BBCharacterService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BreakingBad.Data
+{
+    class BBCharacterService
+    {
+        private static readonly string apiRoot = "https://www.breakingbadapi.com/api";
+        private static readonly string apiCharacterEndoint = apiRoot + "/characters?name=";
+        public static async Task<List<BBCharacter>> GetCharactersFromName(string queryName)
+        {
+            try
+            {
+                var newName = queryName.Trim().Replace(' ', '+');
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(apiCharacterEndoint + newName))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+                            //If the data isn't null return log convert the data using newtonsoft JObject Parse class method on the data.
+                            List<BBCharacter> characters = null;
+                            if (data != null) characters = JsonConvert.DeserializeObject<List<BBCharacter>>(data);
+                            return characters;
+                        }
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
